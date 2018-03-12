cdb1495a62e51d3b1675c8dbd160fef9be5fa55c
Joan <Joan@192.168.1.3>
2018-03-12 14:54:26 -0500

    Version 1.0.1

--- calc/src/main/java/edu/usmp/calc/model/Result.java
+++ calc/src/main/java/edu/usmp/calc/model/Result.java
@@ -2,4 +2,34 @@ package edu.usmp.calc.model;
 
 public class Result {
 
+	private double n1; 
+	private double n2;
+	private double result;
+	private char op;
+	
+	public double getN1() {
+		return n1;
+	}
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+	public double getN2() {
+		return n2;
+	}
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+	public double getResult() {
+		return result;
+	}
+	public void setResult(double result) {
+		this.result = result;
+	}
+	public char getOp() {
+		return op;
+	}
+	public void setOp(char op) {
+		this.op = op;
+	}
+	
 }
--- calc/src/main/java/edu/usmp/calc/web/ResultController.java
+++ calc/src/main/java/edu/usmp/calc/web/ResultController.java
@@ -1,5 +1,25 @@
 package edu.usmp.calc.web;
 
-public class ResultController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import edu.usmp.calc.model.Result;
+
+@Controller
+public class ResultController {
+	@GetMapping("/calcInt")
+	public String initCreationForm(Model model) {
+		model.addAttribute("result", new Result());
+		
+		return "calcForm";
+	}
+	
+	@PostMapping("/calcInt")
+	public String submitForm(@ModelAttribute Result result) {
+		return "resultForm";
+	}
+	
 }
